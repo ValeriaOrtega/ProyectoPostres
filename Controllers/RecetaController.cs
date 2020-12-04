f21d68698b766beb4b30294fdd7fd96c2e3b11bd
ValeriaOrtega <victoria.perez18@tectijuana.edu.mx>
2020-12-04 14:01:17 -0800

    conexiñon a sql

---
+++ Controllers/RecetaController.cs
@@ -0,0 +1,94 @@
+﻿using ProyectoPostres.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPostres.Controllers
+{
+    public class RecetaController : Controller
+    {
+        readonly RecetaDbConnection db = new RecetaDbConnection();
+        // GET: Receta
+        public ActionResult Index()
+        {
+            var receta = db.Recetas.ToList();
+            return View(receta);
+        }
+        // GET: Receta/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Receta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Receta/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Receta/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Receta/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Receta/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            var receta = db.Recetas.Find(id);
+
+            return View(receta);
+        }
+        // POST: Receta/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ Migrations/202012042135373_StartProject.cs
@@ -0,0 +1,28 @@
+﻿namespace ProyectoPostres.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StartProject : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Recetas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre_Postre = c.String(),
+                        Ingredientes = c.String(),
+                        Receta_Postre = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Recetas");
+        }
+    }
+}
